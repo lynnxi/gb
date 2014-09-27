2297d8ef359dbe1854d3b418dc3034f231247015
朱林 <zhu.lin@immomo.com>
2014-09-27 14:33:03 +0800

    up

--- main.go
+++ main.go
@@ -18,22 +18,19 @@ import (
 )
 
 var (
-	wait   sync.WaitGroup
-	c      = flag.Int("c", 100, "concurrency")
-	n      = flag.Int("n", 10, "number")
-	url    = flag.String("h", "", "url")
-	file   = flag.String("f", "", "params file")
-	params []string
+	wait      sync.WaitGroup
+	c         = flag.Int("c", 500, "concurrency")
+	n         = flag.Int("n", 10000, "number")
+	paramFile = flag.String("pf", "", "params file")
+	urlFile   = flag.String("uf", "", "url file")
+	params    []string
+	urls      []string
 )
 
-func main() {
-	runtime.GOMAXPROCS(4)
-
-	flag.Parse()
-
-	fi, err := os.Open(*file)
+func parseParamFile(file string) {
+	fi, err := os.Open(file)
 	if err != nil {
-		fmt.Println(*file)
+		fmt.Println(file)
 		panic(err)
 	}
 	defer fi.Close()
@@ -50,15 +47,46 @@ func main() {
 		}
 		line = line[:i]
 		params = append(params, string(line))
-
 	}
+}
+
+func parseUrlFile(file string) {
+	fi, err := os.Open(file)
+	if err != nil {
+		fmt.Println(file)
+		panic(err)
+	}
+	defer fi.Close()
+	reader := bufio.NewReader(fi)
+
+	for {
+		line, err := reader.ReadSlice('\n')
+		if err == io.EOF {
+			break
+		}
+		i := len(line) - 2
+		if i < 0 {
+			fmt.Println("bad line terminator:" + string(line))
+		}
+		line = line[:i]
+		urls = append(urls, string(line))
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(8)
+
+	flag.Parse()
+
+	parseParamFile(*paramFile)
+	parseUrlFile(*urlFile)
 
 	start := time.Now()
 	for i := 0; i < *c; i++ {
 		wait.Add(1)
 
 		go func() {
-			sendHttp(*url, *n)
+			startHttpTest(*n)
 			wait.Done()
 		}()
 	}
@@ -70,7 +98,17 @@ func main() {
 	fmt.Println("num : ", num, "cost : ", cost, "averange : ", average)
 }
 
-func sendHttp(url string, n int) {
+func startHttpTest(n int) {
+	for i := 0; i < n; i++ {
+		param := params[rand.Intn(len(params))]
+		for _, url := range urls {
+			sendHttp(url, param)
+		}
+
+	}
+}
+
+func sendHttp(url string, param string) {
 	var data map[string]interface{}
 	var d string
 	if strings.Contains(url, "?") {
@@ -78,24 +116,21 @@ func sendHttp(url string, n int) {
 	} else {
 		d = "?"
 	}
-	client := &http.Client{}
 
-	for i := 0; i < n; i++ {
-		param := params[rand.Intn(len(params))]
-		req, err := http.NewRequest("GET", url+d+param, nil)
-		req.Header.Set("User-Agent", "go benchmark v 0.1")
-		res, err := client.Do(req)
-		//res, err := http.Get(url + d + param)
-		if err != nil {
-			fmt.Println("http get error, ", err, res)
-			continue
-		}
-		result, err := ioutil.ReadAll(res.Body)
-		res.Body.Close()
-		if err != nil {
-			fmt.Println("http get error, ", err)
-			continue
-		}
-		json.Unmarshal(result, &data)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url+d+param, nil)
+	req.Header.Set("User-Agent", "go benchmark v 0.1")
+	res, err := client.Do(req)
+	//res, err := http.Get(url + d + param)
+	if err != nil {
+		fmt.Println("http get error, ", err, res)
+		return
 	}
+	result, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		fmt.Println("http get error, ", err)
+		return
+	}
+	json.Unmarshal(result, &data)
 }
